fa5461e8cded61a8a3e301b8654359abc6082e5e
unknown <nakhang2002@gmail.com>
2022-04-21 21:10:14 +0700

    update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const { default: mongoose } = require("mongoose");
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://192.168.7.70/hostmanage');
+//mongoose.connect('mongodb://192.168.7.70/hostmanage');
 
-app.use(user);
-app.use(vm);
+//app.use(user);
+//app.use(vm);
 
 app.listen(3000,() => {
     console.log("server is listening port 3000");
